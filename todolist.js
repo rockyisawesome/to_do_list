fde13c6bd592bb8e1678c3ea445ba36c9cbda82c
chandransh <pandeychandransh@gmail.com>
2021-11-23 20:49:14 +0530

    Initial commit

---
+++ todolist.js
@@ -0,0 +1,99 @@
+let add = document.getElementById('add');   //button ki id he beta ye
+let clear = document.getElementById('clear');
+
+function update(){
+    //console.log("updating..");
+   document.getElementById('title').value = "";
+   document.getElementById('description').value = "";
+
+  
+
+    // if (localStorage.getItem('itemsJson') == null) {
+    //     itemJsonArr = [];
+    //     //itemJsonArr.push([tit, desc]);
+    //     localStorage.setItem('itemsJson', JSON.stringify(itemJsonArr));
+
+    // }
+    // else {
+
+    //     itemJsonArrStr = localStorage.getItem('itemsJson');
+    //     itemJsonArr = JSON.parse(itemJsonArrStr);
+    //     itemJsonArr.push([tit, desc]);
+    //     localStorage.setItem('itemsJson', JSON.stringify(itemJsonArr));
+    // }
+
+    console.log("hello");
+    let  itemJsonArrStr = localStorage.getItem('itemsJson');
+    let  itemJsonArr = JSON.parse(itemJsonArrStr);
+
+
+
+    let tableBody = document.getElementById("tableBody"); 
+    let str = "";
+    itemJsonArr.forEach((element, index) => {
+
+        str += `
+            <tr>
+                <th scope="row">${index+1}</th>
+                <td>${element[0]}</td>
+                <td>${element[1]}</td>
+                <td><button class="btn btn-sm btn-danger" onclick = "deleted(${index})">Delete</button></td>
+            </tr>`;
+    });
+
+
+    console.log("hello");
+    tableBody.innerHTML = str;
+
+
+}
+
+function deleted ( itemIndex){
+
+    itemJsonArrStr = localStorage.getItem('itemsJson');
+    itemJsonArr = JSON.parse(itemJsonArrStr);
+
+    // now delete the index
+    itemJsonArr.splice(itemIndex, 1);
+    localStorage.setItem('itemsJson', JSON.stringify(itemJsonArr));
+    update();
+
+}
+
+
+
+function getUpdate(){
+    console.log("updating..");
+    tit = document.getElementById('title').value;
+    desc = document.getElementById('description').value;
+
+    if(tit = "" || desc == ""){
+        alert("fill the boxes");
+        return;
+    }
+
+    else if (localStorage.getItem('itemsJson') == null) {
+        let itemJsonArr = [];
+        itemJsonArr.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArr));
+
+    }
+    else {
+
+        let itemJsonArrStr = localStorage.getItem('itemsJson');
+        let itemJsonArr = JSON.parse(itemJsonArrStr);
+        itemJsonArr.push([tit, desc]);
+        localStorage.setItem('itemsJson', JSON.stringify(itemJsonArr));
+    }
+
+
+
+    update();
+
+
+}
+update();
+
+add.addEventListener("click", getUpdate);
+//clear.addEventListener("click", )
+
